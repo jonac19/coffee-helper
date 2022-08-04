04f30e1eaa03f269b459860daa5868dc20c952ea
jonac19 <jonac19@uci.edu>
2022-08-03 23:18:05 -0700

    Initial Commit

---
+++ SPRO Coffee Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPRO Coffee Helper
+//
+//  Created by Jonathan Chen on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
