fb2231b92b794899dc9071c0538d3d14d946972c
jonac19 <jonac19@uci.edu>
2022-08-03 23:18:05 -0700

    Initial Commit

---
+++ SPRO Coffee Helper/SPRO_Coffee_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPRO_Coffee_HelperApp.swift
+//  SPRO Coffee Helper
+//
+//  Created by Jonathan Chen on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SPRO_Coffee_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
