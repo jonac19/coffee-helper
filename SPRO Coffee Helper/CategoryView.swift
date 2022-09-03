0017d81beea0faf161c5a3f584c6c43423dc7779
jonac19 <jonac19@uci.edu>
2022-09-02 20:11:32 -0700

    Adjusted colors and changed navigation bar style

--- SPRO Coffee Helper/CategoryView.swift
+++ SPRO Coffee Helper/CategoryView.swift
@@ -12,13 +12,28 @@ struct CategoryView: View {
     @State private var visibleCategories: Set<Category>
     
     init(group: Group) {
+        UIBarButtonItem.appearance().tintColor = .white
         visibleCategories = Set(group.categories)
         self.group = group
     }
     
     var body: some View {
         NavigationView {
-            VStack (spacing: 0) {
+            VStack {
+                // Top navbar
+                HStack {
+                    Image("spro_logo_clear")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100)
+                    Spacer()
+                    Image("search_icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 45)
+                }
+                .padding([.leading, .trailing], 20)
+
                 // Category navbar
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 25) {
@@ -29,19 +44,22 @@ struct CategoryView: View {
                                 } else {
                                     visibleCategories.insert(category)
                                 }
-                            }.foregroundColor(.white)
+                            }
+                            .foregroundColor(.white)
+                            .font(.system(size: 18))
                             
                         }
                     }
                     .padding(EdgeInsets(top: 15, leading: 20, bottom: 15, trailing: 20))
                 }
-                .background(Color(red: 0.60, green: 0, blue: 0))
+                .background(Color(red: 0.9, green: 0.10, blue: 0.02))
                 
                 // Category list
                 List {
                     ForEach(group.categories) { category in
                         if visibleCategories.contains(category) {
-                            Section(header: Text(category.name)) {
+                            Section(header: Text(category.name).foregroundColor(Color(red: 0.16, green: 0.09, blue: 0.07)))
+                            {
                                 ForEach(category.drinks) { drink in
                                     NavigationLink(
                                         drink.name, destination: DrinkView(drink: drink)
